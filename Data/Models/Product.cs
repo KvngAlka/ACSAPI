4bac43312bbbde7d95ad051affe110c52af0a7b1
kashitey <kashitey@acsint.io>
2022-12-15 11:19:10 +0000

    api structured

---
+++ Data/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data.Models
+{
+    internal class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ProductLongName { get; set; }
+        public string ProductCategory { get; set; }
+        public int ShortCode { get; set; }
+        public string BillingType { get; set; }
+        public string ProductStatus { get; set; }
+
+
+
+    }
+}
